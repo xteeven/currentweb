1a1fe88a9baa9e505cddb1a2177a7648da2b53bd
Steeven Villa <david7246@gmail.com>
2022-03-15 20:14:12 +0100

    CleanNotCompiling

--- docs/.vuepress/.temp/register-components/clientAppEnhance.2dd40e15.js
+++
@@ -1,7 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export default ({ app }) => {
-  app.component("Icons", defineAsyncComponent(() => import("C:/Repositories/Web/docs/.vuepress/components/Icons.vue"))),
-  app.component("Paper", defineAsyncComponent(() => import("C:/Repositories/Web/docs/.vuepress/components/Paper.vue"))),
-  app.component("Thesis", defineAsyncComponent(() => import("C:/Repositories/Web/docs/.vuepress/components/Thesis.vue")))
-}
