7b4ac3dd6683467365fcadd75387182939beb4e6
Steeven Villa <david7246@gmail.com>
2023-02-15 20:14:10 +0100

    fix portfolio

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,7 +3,7 @@ import { defineNavbarConfig } from "vuepress-theme-hope";
 export default defineNavbarConfig([
   "/", 
   { text: "Publications", icon: "article", link: "/publications/" }, 
-  { text: "Portfolio", icon: "process", link: "/portfolio/" },
+  //{ text: "Portfolio", icon: "process", link: "/portfolio/" },
   { text: "Teaching", icon: "creative", link: "/teaching/" }, 
   { text: "Contact", icon: "quote", link: "/contact/" }, 
 
